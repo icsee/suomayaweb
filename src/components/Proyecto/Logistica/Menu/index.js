f9aef0cf909e4b601d7ae86b03344306b1885539
juanamezquita <t.amezquita.juancarlos@gmail.com>
2021-07-12 12:54:49 -0500

    Se actualiza proyecto logistica

--- src/components/Proyecto/Logistica/Menu/index.js
+++ src/components/Proyecto/Logistica/Menu/index.js
@@ -7,62 +7,18 @@ const MenuLogistica = () => {
   return (
     <div className="row">
       <nav><ul>
-        <li><a href="#">
-          <NavLink to='/proyecto/logistica'>
-          <button  className='item'>     Logistica</button>    
-         </NavLink>
+        <li><a href="https://siclog.suomaya.co/">
+         
+          <Button link="https://siclog.suomaya.co/">
+                  Aplicación
+              </Button>
+        
+      
         </a></li>
 
-        <li><a href="#"> <button  className='item'>     Bases</button>  </a>
-          <ul><li><a href="">
-            <NavLink to='/logistica/clientes'>
-            <button  className='item'>     Clientes</button>  
-            </NavLink>
-          </a></li>
-            <li><a href="">
-              <NavLink to='/logistica/cliente-crear'>
-              <button  className='item'>      Nuevo Cliente</button>    
-                </NavLink>
-            </a></li>
-            <li><a href="">
-              <NavLink to='/logistica/proveedores'>
-              <button  className='item'>    Proveedores</button>     
-               </NavLink>
+      
 
-            </a></li>
-            <li><a href="">
-              <NavLink to='/logistica/proveedor-crear'>
-              <button  className='item'>    Nuevo Proveedor</button>   
-      </NavLink>
-            </a></li>
-            <li><a href="">
-              <NavLink to='/logistica/almacenes'>
-              <button  className='item'> Almacenes</button>    
-      </NavLink>
-            </a></li>
-            <li><a href="">
-              <NavLink to='/logistica/almacen-crear' className='item'>
-              <button  className='item'> Nuevo Almacen</button>   
-      </NavLink>
-            </a></li>
-            <li>
-              <NavLink to='/logistica/productos' className='item'>
-              <button  className='item'>Productos</button>   
-      </NavLink>
-            </li>
-            <li>
-              <NavLink to='/logistica/producto-crear' className='item'>
-              <button  className='item'>Nuevo Producto</button>
-      </NavLink>
-          </li>
-            
-
-          </ul>
-        </li>
-
-        <li>
-          <NavLink to='/logistica/reporte'> <button  className='item'>Reporte</button></NavLink>
-        </li>
+     
       </ul></nav>
       {/*<Button variant="small" route='proyecto/logistica'>
       Logistica
